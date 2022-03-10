f3315f91bf7e91e98c95361149cba3430b7c8668
gechderib <gechderib@gmail.com>
2022-03-10 16:04:40 +0300

    component added

--- src/components/ui/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css"
-
-function Card1(props){
-    return <div className="card1">
-        {props.children}
-    </div>
-}
-
-export default Card1;
--- src/components/ui/FormWrap.js
+++
@@ -1,9 +0,0 @@
-import "./Formwrap.css"
-
-function FormWrap(props){
-    return <div className="main">
-        {props.children}
-    </div>
-}
-
-export default FormWrap;
